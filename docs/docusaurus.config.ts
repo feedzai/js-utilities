363032bb16b45930377b20749b4377fe02a53416
João Dias <contacto@joaodias.me>
2024-03-18 10:28:54 +0000

    docs(package): added documentation

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,63 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+import PACKAGE_JSON from "../package.json";
+
+const config: Config = {
+  title: PACKAGE_JSON.name,
+  tagline: PACKAGE_JSON.description,
+  url: "https://feedzai.github.io/",
+  baseUrl: "/js-utilities/",
+  organizationName: "@feedzai",
+  projectName: "js-utilities",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+  plugins: ["docusaurus-plugin-sass"],
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "@feedzai/js-utilities",
+      logo: {
+        alt: "",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Docs",
+        },
+        {
+          href: "https://github.com/feedzai/js-utilities",
+          label: "Source Code",
+          position: "right",
+        },
+      ],
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
