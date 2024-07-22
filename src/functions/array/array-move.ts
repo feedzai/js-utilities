f08f6946e8aeea5a466dafd9143dff514b48b268
João Dias <joao.dias@feedzai.com>
2024-07-23 00:21:42 +0100

    docs(arrayMove): added jsdoc examples

--- src/functions/array/array-move.ts
+++ src/functions/array/array-move.ts
@@ -9,6 +9,22 @@
  *
  * - The old position;
  * - and the new position.
+ *
+ * @example
+ * ```js
+ * import { arrayMove } from "@feedzai/js-utilities";
+ *
+ * const DATA = [
+ *  "Homer",
+ *  "Marge",
+ *  "Lisa",
+ *  "Bart",
+ *  "Maggie"
+ * ];
+ *
+ * const SORTED = arrayMove(DATA, 2, 0);
+ * // ["Lisa", "Homer", "Marge", "Bart", "Maggie"];
+ * ```
  */
 export function arrayMove<Source>(array: Source[], fromIndex: number, toIndex: number): Source[] {
   const orderedArray = [...array];
