33bf4d7cb5df8c785b75240bcfec63662ccccee6
João Dias <joao.dias@feedzai.com>
2025-06-04 08:55:13 +0100

    feat(at): added type safety to return value with generics

--- src/functions/object/at.ts
+++ src/functions/object/at.ts
@@ -18,12 +18,15 @@ import { isNull } from "../typed";
  * at(object, ['a[0].b.c', 'a[1]']);
  * // [3, 4]
  */
-export function at(object: any, paths: string | string[]): any[] {
-  const PATHS = Array.isArray(paths) ? paths : [paths];
-  const FLATTENED_PATHS = flatten(PATHS, 1);
-  const PATHS_LENGTH = FLATTENED_PATHS.length;
-  const RESULT = new Array(PATHS_LENGTH);
-  const SHOULD_SKIP = isNull(object);
+export function at<GenericValue>(
+  object: GenericValue | null | undefined,
+  paths: string | string[]
+): Array<GenericValue[keyof GenericValue]> {
+  const normalizedPaths = Array.isArray(paths) ? paths : [paths];
+  const flattenedPaths = flatten(normalizedPaths, 1);
+  const pathsLength = flattenedPaths.length;
+  const result = new Array(pathsLength);
+  const shouldSkip = isNull(object);
 
   /**
    * It iterates over the flattenedPaths array.
@@ -32,9 +35,9 @@ export function at(object: any, paths: string | string[]): any[] {
    *  - If object is null or undefined, it assigns undefined to the result array.
    * The get function is used here to safely retrieve nested properties of the object.
    */
-  for (let index = 0; index < PATHS_LENGTH; index++) {
-    RESULT[index] = SHOULD_SKIP ? undefined : get(object, FLATTENED_PATHS[index]);
+  for (let index = 0; index < pathsLength; index++) {
+    result[index] = shouldSkip ? undefined : get(object, flattenedPaths[index]);
   }
 
-  return RESULT;
+  return result;
 }
