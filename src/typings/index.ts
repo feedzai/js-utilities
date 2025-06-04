5fc769d1927cbd74c3035455189a975a4dd9ff02
João Dias <joao.dias@feedzai.com>
2025-06-04 09:06:16 +0100

    feat(types): removed react-related types

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -3,5 +3,31 @@
  *
  * (c) 2024 Feedzai
  */
-export * from "./generic";
-export * from "./polymorphic";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type GenericAnyFunction = (...args: any) => any;
+
+// https://stackoverflow.com/questions/46176165/ways-to-get-string-literal-type-of-array-values-without-enum-overhead
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const tuple = <T extends string[]>(...args: T) => args;
+
+export type GlobalPartial<T> = Partial<T>;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+export type PartialObject<T> = GlobalPartial<T>;
+export type Many<T> = T | ReadonlyArray<T>;
+export type PropertyName = string | number | symbol;
+export type PropertyPath = Many<PropertyName>;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type ObjTypeWithAny = Record<string, any>;
+export type StringPropertyPath = Many<string>;
+export interface Dictionary<T> {
+  [index: string]: T;
+}
+export interface NumericDictionary<T> {
+  [index: number]: T;
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type PromiseValues<T extends Promise<any>[]> = {
+  [K in keyof T]: T[K] extends Promise<infer U> ? U : never;
+};
